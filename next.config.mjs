27103d29686f466f015df32e1f884ab69155034c
predatorowl3671-dev <predator.owl3671@gmail.com>
2026-02-27 14:25:18 +0900

    変更９

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // 型エラーを無視
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ビルド時のLintエラーを無視
   },
   images: {
     unoptimized: true,
   },
 }
-
 export default nextConfig
